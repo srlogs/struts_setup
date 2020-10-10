31afb6a7681d32bf09504b73adf8cad2e9eb55c6
srlogs <logeshwaran.sr1@gmail.com>
2020-10-10 08:32:59 +0530

    struts setup

---
+++ HelloWorld.jsp
@@ -0,0 +1,9 @@
+<html>
+   <head>
+      <title>Hello World</title>
+   </head>
+   
+   <body>
+      Hello World, <s:property value = "name"/>
+   </body>
+</html>
---
+++ WEB-INF/classes/HelloWorldAction.java
@@ -0,0 +1,18 @@
+//import com.opensymphony.xwork2.Action;
+
+
+public class HelloWorldAction {
+   private String name;
+
+   public String execute() throws Exception {
+      return "success";
+   }
+   
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
